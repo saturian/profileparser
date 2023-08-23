e3477e9359ce0dce21c8d7668da3a09a78509c69
Shukurullayev Elbek <118737003+saturian@users.noreply.github.com>
2023-08-24 00:45:32 +0500

    Create code.py

---
+++ addon/code.py
@@ -0,0 +1,17 @@
+session = input("session: ")
+useri = "vermino"
+
+first_name = "Telegram"
+
+last_name = "Telegtam"
+
+about = "Telegram"
+
+username = "TeLegram"
+
+you_phone = "number"
+
+chat = "saturian_boy"
+#leavechannel, 
+
+msg_id = 5
